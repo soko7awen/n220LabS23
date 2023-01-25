16767aef76c568c447a942842b0630368bbfd7b5
soko Awen <havenhamelin@gmail.com>
2023-01-25 12:30:44 -0500

    Added comments

---
+++ Labs/animcondit/js/app.js
@@ -0,0 +1,25 @@
+// Haven Hamelin
+// 01/25/23
+// N220
+
+//Define posX and posY, along with speedX and speedY, and boundsX and boundsY
+
+
+function setup() {
+    //createCanvas(boundsX, boundsY);
+}
+
+function draw() {
+    //background(255);
+    //Create a circle of arbitrary size with position of (posX,posY)
+
+    //Check if posX has not reached 0 or boundsX, if so add speedX to posX
+    
+    //Otherwise, multiply speedX by -1
+
+    //Check if posY has not reached 0 or boundsY, if so add speedY to posY
+
+    //Otherwise, multiply speedY by -1
+
+
+}
---
+++ js/app.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(255);
+    fill(0);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
