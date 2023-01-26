6853e01de4d81bf3306cf8b8f4bfe8b92cf0b083
soko Awen <havenhamelin@gmail.com>
2023-01-25 19:30:15 -0500

    Updated styling

--- Labs/animcondit/js/app.js
+++ Labs/animcondit/js/app.js
@@ -2,24 +2,35 @@
 // 01/25/23
 // N220
 
-//Define posX and posY, along with speedX and speedY, and boundsX and boundsY
+//Define boundsX and boundsY, posX and posY, along with speedX and speedY
+let boundsX = 800;
+let boundsY = 600;
+let r = 50;
+let posX = r/2;
+let posY = r/2;
+let speedX = 2;
+let speedY = 2;
 
 
 function setup() {
-    //createCanvas(boundsX, boundsY);
+    createCanvas(boundsX, boundsY);
 }
 
 function draw() {
-    //background(255);
+    background(255);
+    noStroke();
+    fill(0,200,40);
     //Create a circle of arbitrary size with position of (posX,posY)
-
-    //Check if posX has not reached 0 or boundsX, if so add speedX to posX
-    
-    //Otherwise, multiply speedX by -1
-
-    //Check if posY has not reached 0 or boundsY, if so add speedY to posY
-
-    //Otherwise, multiply speedY by -1
-
-
+    circle(posX,posY,r);
+    //Check if posX is below 0 or above boundsX, if so multiply speedX by -1
+    if(posX<0+(r/2)||posX>boundsX-(r/2)) {
+        speedX *= -1;
+    }
+    //Check if posX is below 0 or above boundsX, if so multiply speedX by -1
+    if(posY<0+(r/2)||posY>boundsY-(r/2)) {
+        speedY *= -1;
+    }
+    //Add speedX and speedY to posX and posY, respectively
+    posX += speedX;
+    posY += speedY;
 }
