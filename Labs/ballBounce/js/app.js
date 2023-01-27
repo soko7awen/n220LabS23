cc0cd26ad3e869b0b8b1b2b91f32ab063b404f69
soko Awen <havenhamelin@gmail.com>
2023-01-26 22:49:59 -0500

    Changed ball to DVD logo

--- Labs/ballBounce/js/app.js
+++ Labs/ballBounce/js/app.js
@@ -2,15 +2,20 @@
 // 01/25/23
 // N220
 
+let dvd;
+function preload() {
+  dvd = loadImage('img/dvdlogo.png');
+}
+let dvdSize = { x: 187, y: 83 }
+
+
 //Define boundsX and boundsY, posX and posY, along with speedX and speedY
 let boundsX = 800;
 let boundsY = 600;
-let r = 50;
-let posX = r/2;
-let posY = r/2;
+let posX = dvdSize.x/2;
+let posY = dvdSize.y/2;
 let speedX = 5;
 let speedY = 5;
-
 
 function setup() {
     createCanvas(boundsX, boundsY);
@@ -21,13 +26,13 @@ function draw() {
     noStroke();
     fill(0,200,40);
     //Create a circle of arbitrary size with position of (posX,posY)
-    circle(posX,posY,r);
+    image(dvd,posX,posY);
     //Check if posX is below 0 or above boundsX, if so multiply speedX by -1
-    if(posX<0+(r/2)||posX>boundsX-(r/2)) {
+    if(posX<0||posX>boundsX-(dvdSize.x)) {
         speedX *= -1;
     }
     //Check if posX is below 0 or above boundsX, if so multiply speedX by -1
-    if(posY<0+(r/2)||posY>boundsY-(r/2)) {
+    if(posY<0||posY>boundsY-(dvdSize.y)) {
         speedY *= -1;
     }
     //Add speedX and speedY to posX and posY, respectively
