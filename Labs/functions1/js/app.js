d3101c0b07062d902eaa36a047f0a692ee1670b2
soko Awen <havenhamelin@gmail.com>
2023-01-31 12:51:07 -0500

    Updated Functions 1

--- Labs/functions1/js/app.js
+++ Labs/functions1/js/app.js
@@ -3,8 +3,8 @@
 // N220
 
 //Define canvas size,
-//let canvasX = 800;
-//let canvasY = 600;
+let canvasX = 800;
+let canvasY = 600;
 
 
 function setup() {
@@ -15,16 +15,20 @@ function draw() {
     background(255);
     noStroke();
     //Call drawCars() to the position of the mouse
-    drawCars(mouseX,mouseY);
+    drawCars(mouseX+70,mouseY+70,30,70);
+    drawCars(mouseX+70,mouseY-70,30,70);
+    drawCars(mouseX-70,mouseY,30,70);
     //Call drawCars() to the position of the mouse plus some vector
     //Call drawCars() to the position of the mouse plus some other vector
 
 }
-//Declare function drawCars() that takes two arguments, x and y
-function drawCars(x,y) {
-    //Draw wheels
-    circle(x-1,y-1,5);
-    circle(x+1,y+1,5);
+//Declare function drawCars() that takes two arguments, x and y, and r (radius), and w (width)
+function drawCars(x,y,r,w) {
     //Draw car body
-    //Draw window
+    fill(255,0,0);
+    rect(x-w/2,y-r,w,30);
+    //Draw wheels
+    fill(0);
+    circle(x-w/2,y,r);
+    circle(x+w/2,y,r);
 }
