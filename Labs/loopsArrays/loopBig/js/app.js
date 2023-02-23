2dad0a417e59fef3a6920068410d8312b6864aab
soko7awen <havenhamelin@gmail.com>
2023-02-23 18:20:09 -0500

    Update app.js

--- Labs/loopsArrays/loopBig/js/app.js
+++ Labs/loopsArrays/loopBig/js/app.js
@@ -8,5 +8,5 @@ let square = document.getElementById("Square")
 //for loop that starts at 1000, runs when i is less then 5001, and increment by 1000 every time
 for (let i=1000; i < 5001; i += 1000) {
     //sets the square innerHTML as i
-    square.innerHTML = i
+    square.innerHTML += i
 }
