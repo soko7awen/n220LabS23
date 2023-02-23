4f5edbf06c5c3e2e232c0bb51e5ca9eaf2f9dbd5
soko7awen <havenhamelin@gmail.com>
2023-02-23 18:18:27 -0500

    Update app.js

--- Labs/loopsArrays/loopBig/js/app.js
+++ Labs/loopsArrays/loopBig/js/app.js
@@ -3,6 +3,10 @@
 // N220
 
 //square = Get element by id "Square"
+let square = document.getElementById("Square")
 
 //for loop that starts at 1000, runs when i is less then 5001, and increment by 1000 every time
+for (let i=1000; i < 5001; i += 1000) {
     //sets the square innerHTML as i
+    square.innerHTML = i
+}
